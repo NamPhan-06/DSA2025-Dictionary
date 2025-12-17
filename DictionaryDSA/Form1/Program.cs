fb2e0293029e714cf1260e9354ec54b4269531a9
Nam Phan <zonkephan@gmail.com>
2025-12-17 21:39:19 +0700

    Add files via upload

---
+++ DictionaryDSA/Form1/Program.cs
@@ -0,0 +1,26 @@
+﻿using EnglishVietnameseDictionary;
+using System;
+using System.Windows.Forms;
+
+namespace EnglishVietnameseDictionary
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Chuẩn bị giao diện
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+
+            // Lệnh quan trọng nhất: Chạy Form1 lên
+            // Nếu chữ Form1 bị đỏ, hãy đảm bảo Form1.cs của bạn 
+            // đang có namespace là DictionaryDSA
+            Application.Run(new Form1());
+        }
+    }
+}
